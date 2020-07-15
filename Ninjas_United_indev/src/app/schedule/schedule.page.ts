094bca5d5740789b0b9c41ed6d41095caa3d683d
Robin-vl <robin.van.landeghem6@gmail.com>
2020-07-15 21:04:42 +0200

    updated agenda

--- Ninjas_United_indev/src/app/schedule/schedule.page.ts
+++ Ninjas_United_indev/src/app/schedule/schedule.page.ts
@@ -1,6 +1,8 @@
 import { WedstrijdenService } from './../wedstrijden.service';
-import { Component, OnInit } from '@angular/core';
-import { Wedstrijd } from '../wedstrijd';
+import { Component, OnInit, ViewChild, LOCALE_ID, Inject } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar';
+import { AlertController } from '@ionic/angular';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-schedule',
@@ -8,7 +10,8 @@ import { Wedstrijd } from '../wedstrijd';
   styleUrls: ['./schedule.page.scss'],
 })
 export class SchedulePage implements OnInit {
-  eventSource:Wedstrijd []= [];
+  //Firebase nog koppelen
+  eventSource:any []= [];
   calendar = {
     mode: 'month',
     currentDate: new Date(),
@@ -17,45 +20,66 @@ export class SchedulePage implements OnInit {
   onViewTitleChanged(title) {
     console.log(title);
   }
+  event = {
+    title:'',
+    location:'',
+    startTime:'',
+    endTime: '',
+  }
+  minDate = new Date().toISOString()
   maanden:string[]=['Januari','Februari','Maart','April','Mei','Juni','Juli','Augustus','September','Oktober','November','December'];
-  addEvent(){
-    let start = this.selectedDate;
-    let end = this.selectedDate;
-    end.setMinutes(end.getMinutes()+ 60);
-    let event:Wedstrijd ={
-      title: 'Event#: ' + start.getMinutes(),
-      startTime: start.toISOString(),
-      endTime: end.toISOString()
+  
+  @ViewChild(CalendarComponent) myCalend: CalendarComponent;
+  collapseC: boolean = false;
+  ngOnInit() {
+    this.resetEvent();
+  }
+  constructor(private servWed: WedstrijdenService, private alertcntrl: AlertController, @Inject(LOCALE_ID) private loc:string) { }
+  
+  resetEvent(){
+    this.event = {
+      title:'',
+      location:'',
+      startTime: new Date().toISOString(),
+      endTime: new Date().toISOString(),
     }
-    this.servWed.saveWed(event).subscribe(respons=>{
-    })
+  }
+  addGame(){
+    let newEvent = {
+      title: this.event.title,
+      location: this.event.location,
+      startTime: new Date(this.event.startTime),
+      endTime:  new Date(this.event.endTime)
+    }
+    this.eventSource.push(newEvent);
+    this.myCalend.loadEvents();
+    this.resetEvent();
+  }
+  onTimeSelected(event) {
+    let select = new Date(event.selectedTime);
+    this.selectedDate = select;
+    this.event.startTime = select.toISOString();
+    select.setHours(select.getHours()+1);
+    this.event.endTime = select.toISOString();
+  }
+  //Enkel voor volledige dagen te gebruiken
+    async onEventSelected(event) {
+    let start = formatDate(event.startTime,'medium',this.loc);
+    let einde = formatDate(event.endTime,'medium',this.loc);
+    const alert = await this.alertcntrl.create({
+      header: event.title,
+      subHeader: event.location,
+      message: `Startuur: ${start} - Einduur: ${einde}`,
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
-  onEventSelected(event) {
-    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
-  }
 
-  onTimeSelected(ev) {
-    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
-      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
-    this.selectedDate = ev.selectedTime;
-  }
 
   onCurrentDateChanged(event: Date) {
     console.log('current date change: ' + event);
   }
 
-  reloadSource(ev) {
-    console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
-  }
 
-  constructor(private servWed: WedstrijdenService) { }
-
-  ngOnInit() {
-  }
-  ionViewWillEnter(){
-    this.servWed.getWed().subscribe(respons=>{
-      this.eventSource = respons;
-    })
-  }
 }
